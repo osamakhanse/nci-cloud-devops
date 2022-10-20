d063426a8f48cfb202e8782e4475a54531c8e40a
EC2 Default User <ec2-user@ip-172-31-39-124.eu-west-1.compute.internal>
2022-10-20 13:44:35 +0000

    initial commit

--- provider.tf
+++ provider.tf
@@ -1,28 +1,17 @@
-pipeline{
-    agent any
-    
-    tools {
-        terraform 'terraform-1008'
-    }
-    
-    stages{
-        stage('Git Checkout'){
-            steps {
-                git branch: 'main', credentialsId: '306028d0-b816-4ebf-a6a7-00856fd5daf2', url: 'https://github.com/Chetancloud/Terraform-jenkins'
-            }
-        }
-        
-        stage('Terraform Init'){
-            steps{
-                sh label: '',script: 'terraform init'
-            }
-        }
-        
-        stage('Terraform apply'){
-            steps{
-                echo "Terraform action from the parameter is --->${action}"
-                sh label: '',script: 'terraform ${action} --auto-approve'
-            }
-        }
-    }
+provider "aws" {
+  region = "eu-west-1"
+}
+
+ 
+
+resource "aws_instance" "AWS-instance" {
+  count     = 1 
+  ami           = "ami-096800910c1b781ba"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Node1"
+  }
+
+ 
+
 }
