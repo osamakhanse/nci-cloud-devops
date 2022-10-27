7bc11fdfb882e8f92c1a82e486cb22e8454552af
EC2 Default User <ec2-user@ip-172-31-39-124.eu-west-1.compute.internal>
2022-10-27 13:47:10 +0000

    initial commit

--- provider.tf
+++ provider.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "AWS-instance" {
   ami           = "ami-096800910c1b781ba"
   instance_type = "t2.micro"
   tags = {
-    Name = "Node1"
+    Name = "x21242887-sonarcube"
   }
 
  
