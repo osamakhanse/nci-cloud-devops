4758f918c78549bc0b54bb60342a7d7273c7864d
osamakhanse <102169424+osamakhanse@users.noreply.github.com>
2022-11-29 16:53:58 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 resource "aws_instance" "AWS-instance" {
   key_name                = "x21242887-jenkins" 
-  ami                     = "ami-096800910c1b781ba"
+  ami                     = "ami-034f5e483946a7d79"
   vpc_security_group_ids  = ["sg-0fb0b81f7d9210a79"]
   instance_type           = "t2.micro"
   tags = {
